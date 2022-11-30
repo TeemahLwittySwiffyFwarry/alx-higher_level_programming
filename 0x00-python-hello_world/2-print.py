5c1e0c6d64c875d518712e23e843e41b2dc83a5f
TeemahLwittySwiffyFwarry <oladosuoluwayemisi@gmail.com>
2022-11-29 19:35:06 -0800

    Updated task

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,14 +1,2 @@
 #!/usr/bin/python3
 print("\"Programming is like building a multilingual puzzle")
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Tr
